137f2b407a9095d21a79a5c8915efbc605a0d1b7
megadevice83 <megadevice83@gmail.com>
2017-07-18 09:11:18 +0300

    grid

---
+++ src/js/main.js
@@ -0,0 +1,10 @@
+/**
+ * Created by Oshevchuk on 13.07.2017.
+ * http://oshevchuk2016.16mb.com/
+ */
+"use strict";
+var jquery_1 = require("jquery");
+jquery_1["default"](function () {
+    alert(1);
+});
+//# sourceMappingURL=main.js.map
---
+++ src/js/main.ts
@@ -0,0 +1,10 @@
+/**
+ * Created by Oshevchuk on 13.07.2017.
+ * http://oshevchuk2016.16mb.com/
+ */
+
+import $ from "jquery";
+
+$(function () {
+    alert(1);
+})

ee9b71901b9233348bc33de26fa2f2a3f067e404
megadevice83 <megadevice83@gmail.com>
2017-08-04 12:59:34 +0300

    no message

--- build/js/main.js
+++ build/js/main.js
@@ -2,36 +2,28 @@
  * Created by Oshevchuk on 13.07.2017.
  * http://oshevchuk2016.16mb.com/
  */
-
-declare let $;
-
-let provide;
-let data_height=0;
-let data_offset;
-let timespan=13;
-let data_start=8;
-let data_end=21;
-
+var provide;
+var data_height = 0;
+var data_offset;
+var timespan = 13;
+var data_start = 8;
+var data_end = 21;
 $(function () {
-    timespan=data_end-data_start+1;
-    data_height=$('.os-dhx-holder').height();
-    data_offset=$('.os-dhx-holder').offset().top;
-
+    timespan = data_end - data_start + 1;
+    data_height = $('.os-dhx-holder').height();
+    data_offset = $('.os-dhx-holder').offset().top;
     $('.os-dhx-holder').droppable();
     $('.os-event').draggable({
         containment: '#os-root',
         axis: "y",
         drag: function (event, ui) {
-            var offset=$(this).offset();
-            var element_height=$(this).height();
-
-            var pos=(offset.top-data_offset -3);
-            var res=data_height-offset.top;
-
-            var delta=((offset.top-data_offset -3)/(data_height))*timespan;
-
-            var el=$(this).find('.os-title');
-            el.html(Math.round(delta)+data_start);
+            var offset = $(this).offset();
+            var element_height = $(this).height();
+            var pos = (offset.top - data_offset - 3);
+            var res = data_height - offset.top;
+            var delta = ((offset.top - data_offset - 3) / (data_height)) * timespan;
+            var el = $(this).find('.os-title');
+            el.html(Math.round(delta) + data_start);
             console.log(res, data_height, offset.top);
         }
     }).resizable({
@@ -42,36 +34,32 @@ $(function () {
             $(this).css('width', '');
         }
     });
-
     provide = new DateProvide(new Date());
     provide.ShowHeaders();
     // provide.ShowHeaders();
     // provide.NextWeek(); п
     // provide.NextWeek();
     // provide.ShowHeaders();
-
     // console.log(1);
-
     $('.os-dhx-holder').dblclick(function (e) {
         $('.os-modal-overlay').fadeIn(400);
         $(e.target).html(' <div class="os-event">            <div class="os-title">03:05-04:55</div>        <span>Test mission for mission is imposible to posible</span>        <div class="os-resize"></div>        <div class="os-controlls">        <a href="#"><i class="fa fa-pencil" aria-hidden="true"></i></a>        <a href="#"><i class="fa fa-calendar" aria-hidden="true"></i></a>       <a class="os-remove"><i class="fa fa-trash" aria-hidden="true"></i></a>            </div>            </div>')
             .find('.os-event')
             .draggable({
-                containment: '#os-root',
-                axis: "y",
-                drag: function (event, ui) {
-                }
-            }).resizable({
+            containment: '#os-root',
+            axis: "y",
+            drag: function (event, ui) {
+            }
+        }).resizable({
             containment: '#os-root',
             grid: [20, 20],
             resize: function (event, ui) {
                 $(this).css('width', '');
             }
         })
-            .css({"top": e.offsetY});
+            .css({ "top": e.offsetY });
     });
 });
-
 $('.os-dhx-holder').on('click', '.os-remove', function (e) {
     $(this).parent().parent().remove();
 });
@@ -83,118 +71,86 @@ $('.os-dhx-holder').on('click', '.os-event', function (e) {
 $('.os-close-modal').on('click', function () {
     $('.os-modal-overlay').fadeOut(400);
 });
-
 function nextWeek() {
     provide.NextWeek();
 }
-
 function prevWeek() {
     provide.NextWeek(-1);
 }
-
 $(".er").show();
-
-
-class DateProvide {
-    static days = ["Sun", 'Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat', 'Sun'];
-    static mons = ["January", 'February', 'March', 'Aprill', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December'];
-
-    private related_day;
-    public Curent_day;
-
-    constructor(date) {
+var DateProvide = (function () {
+    function DateProvide(date) {
         this.related_day = new Date();
         this.Curent_day = date ? date : new Date();
     }
-
-    public ShowHeaders() {
+    DateProvide.prototype.ShowHeaders = function () {
         // let related_day = this.related_day;
-
         //day of fisrst day on week
-        let cur_day = this.Curent_day.getDay() == 0 ? 7 : this.Curent_day.getDay();
+        var cur_day = this.Curent_day.getDay() == 0 ? 7 : this.Curent_day.getDay();
         this.related_day.setDate(this.Curent_day.getDate() - cur_day + 1);
-
-        let related_day = this.related_day;
-        let start = new Date(related_day);
-
-
+        var related_day = this.related_day;
+        var start = new Date(related_day);
         // let day = this.Curent_day.getDay();
-        $('.os-head-day').eq(cur_day - 1).css({'color': 'red', 'background-color': 'rgb(220,220,220)'});
-
+        $('.os-head-day').eq(cur_day - 1).css({ 'color': 'red', 'background-color': 'rgb(220,220,220)' });
         // let tmp=this.related_day;
         $('.os-head-day').each(function (a) {
-
             $(this).html(DateProvide.days[a + 1] + ", " + related_day.getDate() + "-" + DateProvide.mons[related_day.getMonth()] + "-" + related_day.getFullYear());
             related_day.setDate(related_day.getDate() + 1);
         });
-        let tmp = new Date();
+        var tmp = new Date();
         tmp.setDate(related_day.getDate() - 1);
         // console.log(related_day);
         // console.log(this.Curent_day);
         // $('.os-cal-date').html($('.os-head-day:first-child').html().substr(5) + '-' + $('.os-head-day:last-child').html().substr(5))
         $('.os-cal-date').html(start.getDate() + " " + DateProvide.mons[start.getMonth()] + " " + start.getFullYear() + " - " + tmp.getDate() + " " + DateProvide.mons[tmp.getMonth()] + " " + tmp.getFullYear());
         // console.log(start, tmp);
-    }
-
-    public NextWeek(factor) {
+    };
+    DateProvide.prototype.NextWeek = function (factor) {
         factor = factor ? factor : 1;
         this.Curent_day.setDate(this.Curent_day.getDate() + 7 * factor);
         this.ShowHeaders();
+    };
+    DateProvide.days = ["Sun", 'Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat', 'Sun'];
+    DateProvide.mons = ["January", 'February', 'March', 'Aprill', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December'];
+    return DateProvide;
+}());
+var Day = (function () {
+    function Day() {
+        this.start = 9;
+        this.end = 18;
+        this.events = [];
     }
-}
-
-class Day{
-    start:number=9;
-    end:number=18;
-    public Day:Date;
-    public events:CalendarEvent[];
-
-    constructor(){
-        this.events=[];
-    }
-
-    addEvent(event:CalendarEvent){
+    Day.prototype.addEvent = function (event) {
         this.events.push(event);
-    }
-    
-    editEventTime(event:CalendarEvent, start, end){
-        let index=this.events.indexOf(event);
-        if(index> -1){
-            
+    };
+    Day.prototype.editEventTime = function (event, start, end) {
+        var index = this.events.indexOf(event);
+        if (index > -1) {
         }
-    }
-
-    removeEvent(event:CalendarEvent){
-        let index=this.events.indexOf(event);
-        if(index>-1) {
+    };
+    Day.prototype.removeEvent = function (event) {
+        var index = this.events.indexOf(event);
+        if (index > -1) {
             this.events.splice(index, 1);
         }
-    }
-
-    checkAviability(){
-
-    }
-}
-
-class CalendarEvent {
-    public id:Number;
-    public startData:Date;
-    public endData:Date;
-    public description:string;
-
-    constructor(startData:Date, endData:Date, description?:string) {
+    };
+    Day.prototype.checkAviability = function () {
+    };
+    return Day;
+}());
+var CalendarEvent = (function () {
+    function CalendarEvent(startData, endData, description) {
         this.startData = startData;
         this.endData = endData;
         this.description = description ? description : '';
     }
-}
-
-class Modal {
-    constructor() {
-
+    return CalendarEvent;
+}());
+var Modal = (function () {
+    function Modal() {
     }
-
-    public Show() {
-
-    }
-}
+    Modal.prototype.Show = function () {
+    };
+    return Modal;
+}());
+//# sourceMappingURL=main.js.map
